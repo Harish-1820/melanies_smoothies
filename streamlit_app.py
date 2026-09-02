73b7857b546d38cf2258abd269ba7866079e35f5
Harish-1820 <harishmalviya108@gmail.com>
2026-09-02 13:47:17 +0530

    Correct use_container_width parameter case
    Fix capitalization of use_container_width parameter.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ session = cnx.session()
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
 ).select(
-    col("FRUIT_NAME"), col('SEARCH_ON')
+    col('FRUIT_NAME'), col('SEARCH_ON')
 )
-st.dataframe(data=my_dataframe, use_container_width=true)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
